e4a70ceb214605f773ab4d8f154cc87967cf211a
George Norfolk <george.norfolk@lexisnexis.co.uk>
2020-08-09 21:37:28 +0100

    Fixing evaluate_target_health error.

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -21,5 +21,6 @@ resource "aws_route53_record" "cv" {
     # zone_id for eu-west-1 s3 comes from here: https://docs.aws.amazon.com/general/latest/gr/s3.html
     name    = "cv.norfolkgaming.com.s3-website-eu-west-1.amazonaws.com"
     zone_id = "Z1BKCTXD74EZPE"
+    evaluate_target_health  = false
   }
 }
