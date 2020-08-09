331d51a18195b3b35c30c291738a3e79eed04534
George Norfolk <george.norfolk@lexisnexis.co.uk>
2020-08-09 21:27:17 +0100

    Creating r53 alias record for bucket.

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,8 +1,18 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "cv.norfolkgaming.com"
   acl    = "public-read"
-  # policy = file("policy.json")
   website {
     index_document = "cv.pdf"
+  }
+}
+
+resource "aws_route53_record" "cv" {
+  zone_id = ""
+  name    = "cv.norfolkgaming.com"
+  type    = "A"
+  alias {
+    # zone_id for eu-west-1 s3 comes from here: https://docs.aws.amazon.com/general/latest/gr/s3.html
+    name    = "cv.norfolkgaming.com.s3-website-eu-west-1.amazonaws.com"
+    zone_id = "Z1BKCTXD74EZPE"
   }
 }
