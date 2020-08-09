78acdfa5a698bc931c2e323cf018b8615cd99d2e
George Norfolk <george.norfolk@lexisnexis.co.uk>
2020-08-09 22:00:26 +0100

    Adding cv source from build step, updating name, and setting to detect file change.

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -2,15 +2,16 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "cv.norfolkgaming.com"
   acl    = "public-read"
   website {
-    index_document = "cv.pdf"
+    index_document = "cv_13.pdf"
   }
 }
 
 resource "aws_s3_bucket_object" "object" {
   bucket  = "cv.norfolkgaming.com"
-  key     = "cv.pdf"
-  content = ""
+  key     = "cv_13.pdf"
+  source  = "../../tex/cv_13.pdf"
   acl     = "public-read"
+  etag    = filemd5("../../tex/cv_13.pdf")
 }
 
 resource "aws_route53_record" "cv" {
